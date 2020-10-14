045e5cf73b4f88befd1c7ae701ba60b583133aae
KapitanD <ddden97@mail.ru>
2020-10-14 21:46:20 +0300

    Homework 1: - Realized modules: Features, Footer, Details, Calendar

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import Header from './components/Header/Header.jsx'
+import Main from './components/Main/Main.jsx'
+import Features from './components/Features/Features.jsx'
+import Footer from './components/Footer/Footer.jsx'
+import Calendar from './components/Calendar/Calendar.jsx'
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <>
+            <Header/>
+            <Main/>
+            <Features/>
+            <Footer/>
+        </>
+    );
 }
 
 export default App;
